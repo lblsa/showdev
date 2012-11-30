3a0352c65541f673b1208dbfa6918be15212ef2b
Dasha <dasha@dasha.(none)>
2012-11-30 16:57:34 +0400

    функционал формы

--- src/Acme/DemoBundle/Controller/WelcomeController.php
+++ src/Acme/DemoBundle/Controller/WelcomeController.php
@@ -4,6 +4,7 @@ namespace Acme\DemoBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Acme\DemoBundle\Entity\SignUp;
+use Acme\DemoBundle\Entity\User;
 use Symfony\Component\HttpFoundation\Request;
 
 class WelcomeController extends Controller
@@ -16,16 +17,27 @@ class WelcomeController extends Controller
          *
          */
 
-		$user = new SignUp();
+		$sign = new SignUp();
 		
-		$form = $this->createFormBuilder($user)
+		$form = $this->createFormBuilder($sign)
 			->add('mail', 'text')
 			->add('phone', 'text')
 			->getForm();
 
 		if ($request->getMethod() == 'POST') 
 		{
-			$form->bindRequest($request);			
+			$form->bindRequest($request);	
+			
+			$user = new User();
+			//echo '<pre>'; print_r($_POST['form']['mail']); echo '</pre>';exit;
+			$user->setMail($_POST['form']['mail']);
+			$user->setPhone($_POST['form']['phone']);
+			
+			//добавили в базу
+			$em = $this->getDoctrine()->getEntityManager();
+			$em->persist($user);
+			$em->flush();
+			exit;
 			if ($form->isValid()) 
 			{
 				return $this->redirect($this->generateUrl('welcome_success'));
