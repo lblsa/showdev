3432365f1f1499090441ffa23897c28f3d1db887
Dasha <dasha@dasha.(none)>
2012-12-03 13:35:32 +0400

    рассылка писем при регистрации нового пользователя

--- src/Acme/DemoBundle/Controller/WelcomeController.php
+++ src/Acme/DemoBundle/Controller/WelcomeController.php
@@ -28,18 +28,57 @@ class WelcomeController extends Controller
 		{
 			$form->bindRequest($request);	
 			
-			$user = new User();
-			//echo '<pre>'; print_r($_POST['form']['mail']); echo '</pre>';exit;
-			$user->setMail($_POST['form']['mail']);
-			$user->setPhone($_POST['form']['phone']);
 			
-			//добавили в базу
-			$em = $this->getDoctrine()->getEntityManager();
-			$em->persist($user);
-			$em->flush();
-			exit;
-			if ($form->isValid()) 
+			//if ($form->isValid()) 
 			{
+				$user = new User();
+				//echo '<pre>'; print_r($_POST['form']['mail']); echo '</pre>';exit;
+				$user->setMail($_POST['form']['mail']);
+				$user->setPhone($_POST['form']['phone']);
+				
+				//добавили в базу
+				//$em = $this->getDoctrine()->getEntityManager();
+				//$em->persist($user);
+				//$em->flush();
+				
+				//отправляем уведомление на почту
+				$to= "roman.efimushkin@gmail.com" . ", "."vladimir.stasevich@gmail.com";
+				$subject = "Зарегистрирован новый пользователь";
+				$message = "
+				<html>
+					<head>
+					 <title>Зарегистрирован новый пользователь на showdev.ru</title>
+					</head>
+					<body>
+						<h1>Зарегистрирован новый пользователь на showdev.ru</h1>
+						<table>
+							<tr>
+								<td style='font-weight: bold'>
+									Телефон
+								</td>
+								<td>
+								".$user->getPhone()."
+								</td>
+							</tr>
+							<tr>
+								<td style='font-weight: bold'>
+									E-mail
+								</td>
+								<td>
+								".$user->getMail()."
+								</td>
+							</tr>
+						</table>
+					</body>
+				</html>
+				";
+
+				$headers= "MIME-Version: 1.0\r\n";
+				$headers .= "Content-type: text/html; charset=iso-8859-1\r\n";
+				
+				mail($to, $subject, $message, $headers);
+				
+			//echo '<pre>'; print_r($message); echo '</pre>';exit;
 				return $this->redirect($this->generateUrl('welcome_success'));
 			}
 		}
@@ -54,6 +93,7 @@ class WelcomeController extends Controller
 		
 	public function successAction()
     {
-		 return $this->render('AcmeDemoBundle:Welcome:success.html.twig');
+		$route = $this->getRequest()->attributes->get('_route');
+		return $this->render('AcmeDemoBundle:Welcome:success.html.twig', array('route' => $route));
     }
 }
